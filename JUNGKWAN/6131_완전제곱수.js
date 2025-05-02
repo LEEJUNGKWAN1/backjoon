bc0144b87a99ef54d342a84ae30ec7d476ec2d70
LEEJUNGKWAN1 <wjdrhks1996@naver.com>
2025-05-02 12:13:34 +0900

    2309_일곱난쟁이
    2309_일곱난쟁이

--- JUNGKWAN/6131_완전제곱수.js
+++ JUNGKWAN/6131_완전제곱수.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const input = fs.readFileSync("QUIZ/6131.txt").toString().split("\n");
+const input = fs.readFileSync("JUNGKWAN/6131.txt").toString().split("\n");
 //const input = fs.readFileSync('/dev/stdin').toString().split("\n");
 
 console.log(baekJoon(parseInt(input)));
